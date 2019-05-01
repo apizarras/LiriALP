f8c93a2da8997eadb6c0eff0640c545660bcb489
apizarras <angelalee.p@gmail.com>
2019-04-30 19:56:24 -0500

    installed dotenv package.json

---
+++ assets/js/liri.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const fs = require("fs");
+
+const keys = require("./keys.js");
+
+const spotify = new Spotify(keys.spotify);
+
+const command = process.argv[2];
+const topic = process.argv[3];
+
+const spotifyUrl = https://api.spotify.com;
+
