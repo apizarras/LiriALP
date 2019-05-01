6a36c6ccf953808bddd794e8812c21d9528b55c4
apizarras <angelalee.p@gmail.com>
2019-05-01 18:58:29 -0500

    installed axios

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -4,10 +4,18 @@ const fs = require("fs");
 
 const keys = require("./keys.js");
 
+const axios = require("axios");
+
+const inquirer = requqire("inquirer");
+
 const spotify = new Spotify(keys.spotify);
 
 const command = process.argv[2];
 const topic = process.argv[3];
 
-const spotifyUrl = https://api.spotify.com;
+// const spotifyUrl = https://api.spotify.com;
+
+//axios.get("url") for ticket master, spotify, omdb
+
+
 
