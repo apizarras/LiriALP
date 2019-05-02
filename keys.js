a30efbd0df7537e35c48425ec47960af4374b472
apizarras <angelalee.p@gmail.com>
2019-05-02 13:59:22 -0500

    functionality for spotify

--- keys.js
+++ keys.js
@@ -4,8 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
 exports.ticketmaster = {
   id: process.env.TICKETMASTER_KEY,
   secret: process.env.TICKETMASTER_SECRET
 };
+
+const ticketmasterKey = process.env.TICKETMASTER_KEY;
+
+
--- liri.js
+++ liri.js
@@ -5,12 +5,17 @@ const fs = require("fs");
 const keys = require("./keys.js");
 
 const axios = require("axios");
+const axiosDebug = require('axios-debug')(axios);
 
 const inquirer = require("inquirer");
 
 const moment = require("moment");
 
-// const spotify = new Spotify(keys.spotify);
+const ticketmaster = require("ticketmaster");
+
+const Spotify = require("node-spotify-api");
+
+const spotify = new Spotify(keys.spotify);
 
 //inquirer to ask the user to select a command
 //inquirer to ask the user to select option(artist, song, movie, etc.)
@@ -29,9 +34,7 @@ inquirer
 ])
     .then(function(search) {
      if(search.searchType==="search-concerts") {
-        console.log("selected concerts");
-        // const tmKey = process.env.Consumer_Key;
-        // const tmSecret = process.env.Consumer_Secret;
+        console.log("user selected concerts");
         inquirer
             .prompt([
             {
@@ -47,37 +50,65 @@ inquirer
                 } else {
                     const event = response.event;
                     console.log("upcoming shows for " + event);
-                    console.log(keys);
-                    axios
-                        .get("https://app.ticketmaster.com/discovery/v2/events.json?apikey="+"key goes here").then(function (response) {
-                        console.log(response);
-                })
-        .catch(function(err) {
-            console.log("This err is from the getEventData function");
-        });
-            console.log("line after get function")
-                    }
-                })
-                .catch(function(err) {
-                    console.log("Please check your spelling and try again.");
-                })
+                    getEventData();
+                    console.log("line after get function")
+            }
+        })
+            .catch(function(err) {
+                console.log("This err is from the catch after response.event");
+            });
         };
+        if(search.searchType==="search-songs") {
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        name: "trackName",
+                        message: "What is the song title you would like to search?"
+                    }
+                ])
+                .then(function(response) {
+                    if(response.event==="") {
+                        const trackName = "All You Need Is Love"
+                        console.log(trackName);
+                    } else {
+                        const trackName = response.trackName;
+                        console.log("spotify song data " + trackName);
+                        getSongData();
+                        console.log("line after get function")
+                }
+            })
+            .catch(function(err) {
+                console.log("This err is from the catch after response.event");
+            });
+        }
     });
 
 function getEventData() {
+    console.log("getEventData function started");
+    console.log(TICKETMASTER_KEY);
     axios
-        .get("https://app.ticketmaster.com/discovery/v2/events.json?apikey="+ticketmaster.Consumer_Key).then(function (response) {
-            console.log(response);
-            // for (i = 0; i < 5; i++) {
-            //     console.log("Concert Venue: " + response.data[i].venue.name);
-            //     console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country)
-            //     console.log("Date: " + moment(response.data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a") + "\n");
-            // }
+        .get("https://app.ticketmaster.com/discovery/v2/events.json?keyword="+event+"&apikey="+TICKETMASTER_KEY).then(response=> {
+            console.log(response.data);
         })
-        .catch(function(err) {
-            console.log("This err is from the getEventData function");
+        .catch(err => {
+            console.log("This err is from the getEventData function"+err.response);
         });
     };
+
+    function getSongData() {
+        spotify
+        .search({ type: 'track', query: trackName })
+        .then(function (response) {
+            console.log('Title: ' + response.tracks.items[0].name);
+            console.log('Artist: ' + response.tracks.items[0].artists[0].name);
+            console.log('From the album: ' + response.tracks.items[0].album.name);
+            console.log('Open in browser: ' + response.tracks.items[0].external_urls.spotify)
+        })
+        .catch(function (err) {
+            console.log("Please check your spelling and try again.");
+        });
+    }
 
 
     
