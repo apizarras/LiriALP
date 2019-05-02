3f66504ba5db30e791164debb8c52532f9f6e187
apizarras <angelalee.p@gmail.com>
2019-05-01 22:49:57 -0500

    working through key references

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -18,18 +18,20 @@ const moment = require("moment");
 
 //axios.get("url") for ticket master, spotify, omdb
 
-inquirer.prompt([
+inquirer
+    .prompt([
     {
         type: "list",
         name: "searchType",
         message: "What would you like to search for?",
         choices: ["search-concerts", "search-songs", "search-movies", "feeling-lucky"]
     }
-]).then(function(search){
-    if(search.searchType==="search-concerts") {
+])
+    .then(function(search) {
+     if(search.searchType==="search-concerts") {
         console.log("selected concerts");
-        const tmKey = process.env.Consumer_Key;
-        const tmSecret = process.env.Consumer_Secret;
+        // const tmKey = process.env.Consumer_Key;
+        // const tmSecret = process.env.Consumer_Secret;
         inquirer
             .prompt([
             {
@@ -37,24 +39,34 @@ inquirer.prompt([
                 name: "event",
                 message: "What artist or band are you searching for?"
             }
-            // .then(function(response) {
-            //     if(response.event==="") {
-            //         const event = "Houston Symphony"
-            //     } else {
-            //         const event = response.event;
-            //         console.log("upcoming shows for " + artist)
-            //         }
-            //     }).catch(function(err) {
-            //         console.log("Please check your spelling and try again.")
-            //     })
-            ])
-            
+        ])
+            .then(function(response) {
+                if(response.event==="") {
+                    const event = "Houston Symphony"
+                    console.log(event);
+                } else {
+                    const event = response.event;
+                    console.log("upcoming shows for " + event);
+                    console.log(keys.ticketmaster);
+                    axios
+                        .get("https://app.ticketmaster.com/discovery/v2/events.json?apikey="+"key goes here").then(function (response) {
+                        console.log(response);
+                })
+        .catch(function(err) {
+            console.log("This err is from the getEventData function");
+        });
+            console.log("line after get function")
+                    }
+                })
+                .catch(function(err) {
+                    console.log("Please check your spelling and try again.");
+                })
         };
     });
 
 function getEventData() {
     axios
-        .get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=LYEpAwNS78iCrHrj3zeGKsz8gvM5R1au").then(function (response) {
+        .get("https://app.ticketmaster.com/discovery/v2/events.json?apikey="+ticketmaster.Consumer_Key).then(function (response) {
             console.log(response);
             // for (i = 0; i < 5; i++) {
             //     console.log("Concert Venue: " + response.data[i].venue.name);
@@ -63,11 +75,12 @@ function getEventData() {
             // }
         })
         .catch(function(err) {
-            console.log("Please check your spelling and try again.");
+            console.log("This err is from the getEventData function");
         });
     };
 
 
+    
 
 
 
